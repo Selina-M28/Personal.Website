206c9056b450dcbeda718e342ebdf293a5bb9503
Selina <selina.mena@gmail.com>
2025-03-21 10:07:31 -0400

    initial push with file structure set up

---
+++ src/pages/About.jsx
@@ -0,0 +1,113 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div><main className="main-content">
+    <h2>Welcome To My Personal Page!</h2>
+    <p className="header-p">
+      Some of my passions include fitness, cooking, and traveling
+    </p>
+    <section className="section-content">
+      <h3>Fitness</h3>
+      <hr />
+      <div>
+        <img
+          src="../img/Fitness Photo.jpg"
+          alt="Person doing Yoga"
+          width="155"
+        />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
+          ipsum obcaecati quaerat repudiandae atque ullam officia? Nostrum in
+          obcaecati, eveniet maxime reprehenderit ut soluta sunt beatae
+          dignissimos minus distinctio quod!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+          explicabo delectus, ab quas fugiat, nisi accusantium suscipit quae
+          dolorem eum unde aliquam, nulla doloribus atque at dolor iure
+          voluptatem eaque.Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Deleniti explicabo delectus, ab quas fugiat, nisi accusantium
+          suscipit quae dolorem eum unde aliquam, nulla doloribus atque at
+          dolor iure voluptatem eaque.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+          explicabo delectus, ab quas fugiat, nisi accusantium suscipit quae
+          dolorem eum unde aliquam, nulla doloribus atque at dolor iure
+          voluptatem eaque.Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Deleniti explicabo delectus, ab quas fugiat, nisi accusantium
+          suscipit quae dolorem eum unde aliquam, nulla doloribus atque at
+          dolor iure voluptatem eaque.
+        </p>
+      </div>
+    </section>
+    <section className="section-content">
+      <h3>Cooking</h3>
+      <hr />
+      <div>
+        <img
+          src="../img/Cooking Photo.jpg"
+          alt="Photo of Vegtables"
+          width="350"
+        />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
+          ipsum obcaecati quaerat repudiandae atque ullam officia? Nostrum in
+          obcaecati, eveniet maxime reprehenderit ut soluta sunt beatae
+          dignissimos minus distinctio quod!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+          explicabo delectus, ab quas fugiat, nisi accusantium suscipit quae
+          dolorem eum unde aliquam, nulla doloribus atque at dolor iure
+          voluptatem eaque.Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Deleniti explicabo delectus, ab quas fugiat, nisi accusantium
+          suscipit quae dolorem eum unde aliquam, nulla doloribus atque at
+          dolor iure voluptatem eaque.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+          explicabo delectus, ab quas fugiat, nisi accusantium suscipit quae
+          dolorem eum unde aliquam, nulla doloribus atque at dolor iure
+          voluptatem eaque.
+        </p>
+      </div>
+    </section>
+    <section className="section-content">
+      <h3>Traveling</h3>
+      <hr />
+      <div>
+        <img
+          src="../img/Travel Photo.jpg"
+          alt="buildings in South of France"
+          width="170"
+        />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
+          ipsum obcaecati quaerat repudiandae atque ullam officia? Nostrum in
+          obcaecati, eveniet maxime reprehenderit ut soluta sunt beatae
+          dignissimos minus distinctio quod!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+          explicabo delectus, ab quas fugiat, nisi accusantium suscipit quae
+          dolorem eum unde aliquam, nulla doloribus atque at dolor iure
+          voluptatem eaque.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+          explicabo delectus, ab quas fugiat, nisi accusantium suscipit quae
+          dolorem eum unde aliquam, nulla doloribus atque at dolor iure
+          voluptatem eaque.Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Deleniti explicabo delectus, ab quas fugiat, nisi accusantium
+          suscipit quae dolorem eum unde aliquam, nulla doloribus atque at
+          dolor iure voluptatem eaque.
+        </p>
+      </div>
+    </section>
+  </main></div>
+  )
+}
+
+export default About
---
+++ src/pages/Home.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+function home() {
+  return (
+    <main className="main-content-2">
+        <h2>Former Marketing professional aspiring Software Developer</h2>
+        <section className="section-content">
+          <h3>Passionate, Driven, Hardworking</h3>
+          <hr />
+          <div>
+            <img src="./img/Personal photo.jpg" alt="Selina Mena" width="300" />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
+              ipsum obcaecati quaerat repudiandae atque ullam officia? Nostrum in
+              obcaecati, eveniet maxime reprehenderit ut soluta sunt beatae
+              dignissimos minus distinctio quod!
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+              explicabo delectus, ab quas fugiat, nisi accusantium suscipit quae
+              dolorem eum unde aliquam, nulla doloribus atque at dolor iure
+              voluptatem eaque.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+              explicabo delectus, ab quas fugiat, nisi accusantium suscipit quae
+              dolorem eum unde aliquam, nulla doloribus atque at dolor iure
+              voluptatem eaque.
+            </p>
+          </div>
+        </section>
+      </main>
+  )
+}
+
+export default home
