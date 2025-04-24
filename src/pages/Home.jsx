865db8ecfc63c793b20cdc27e4423c40a5985493
Selina <selinamena3@gmail.com>
2025-04-23 22:26:39 -0400

    added to home page and updated CSS

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import Header from '../Components/Header';
-import imageSrc from '../photos/personal photo.jpg';
-import Footer from '../Components/Footer';
+import React from "react";
+import Header from "../Components/Header";
+import imageSrc from "../photos/personal photo.jpg";
+import Footer from "../Components/Footer";
 
 function Home() {
   return (
@@ -14,17 +14,31 @@ function Home() {
           <h3>Passionate, Driven, Hardworking</h3>
           <hr />
           <div className="intro-container">
-            <img
-              src={imageSrc}
-              alt="Selina Mena"
-              className="intro-image"
-            />
+            <img src={imageSrc} alt="Selina Mena" className="intro-image" />
             <div className="intro-text">
               <p>
-                Software Engineer with expertise in front and back end web development and 8 years of prior experience in digital advertising. Skilled in collaborating with cross-functional teams, data-driven decision-making, and adapting quickly to new challenges. Passionate about learning and using marketing insights to solve technical problems in innovative ways.
+                Software Engineer with expertise in front and back end web
+                development and 8 years of prior experience in digital
+                advertising. Skilled in collaborating with cross-functional
+                teams, data-driven decision-making, and adapting quickly to new
+                challenges. Passionate about learning and using marketing
+                insights to solve technical problems in innovative ways.
               </p>
               <p>
-                <strong>Languages & Tools:</strong> JavaScript | HTML | CSS | PostgreSQL | Express | Node.js | React | Git | REST APIs | GitHub | SQL
+                I'm actively seeking my first opportunity in software
+                engineering and am excited to bring my drive, curiosity, and
+                adaptability to a dynamic team. While I may not have years of
+                experience in tech, I’ve built a strong foundation through
+                hands-on projects during an immersive coding bootcamp. I’m quick
+                to grasp new concepts, thrive in fast-paced environments, and
+                bring a positive, team-first mindset to everything I do. Most
+                importantly, I have a deep passion for building, learning, and
+                continuously growing in this field. Lets connect!
+              </p>
+              <p>
+                <strong>Languages & Tools:</strong> JavaScript | HTML | CSS |
+                PostgreSQL | Express | Node.js | React | Git | REST APIs |
+                GitHub | SQL
               </p>
             </div>
           </div>
